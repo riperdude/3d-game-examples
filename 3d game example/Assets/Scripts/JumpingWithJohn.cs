d4c0ef97eb469c4873c986078ca33b82e3d71c0f
riperdude <isaacrosas334@gmail.com>
2024-02-06 12:01:07 -0800

    bgrtwjhhbt

--- 3d game example/Assets/Scripts/JumpingWithJohn.cs
+++ 3d game example/Assets/Scripts/JumpingWithJohn.cs
@@ -1,25 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class JumpingWithJohn : MonoBehaviour
 {
     public float JumpForce = 10f;
     public float GravityModifier = 1f;
     public bool IsOnGround = true;
+    public Transform checkointAreaObject;
+    public bool isAtCheckpoint = false;
     public float turnSpeed = 7f;
+    public float outOfBounds = -10;
     public float movespeed = 8f;
-    private Rigidbody m_Rigidbody;
     private Vector3 _Movement;
     private Quaternion m_Rotation = Quaternion.identity;
     private Rigidbody _playerRb;
+    private Vector3 _defaultGravity = new Vector3(0f, -9.81f, 0f);
+    private Vector3 _startingPosition;
 
     void Start ()
     {
-        m_Rigidbody = GetComponent<Rigidbody>();
+        _playerRb = GetComponent<Rigidbody>();
         Physics.gravity *= GravityModifier;
     }
     
+    void Update ()
+    {
+         if(Input.GetKeyDown(KeyCode.Space) && IsOnGround)
+        {
+            _playerRb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
+            IsOnGround = false;
+        }
+        if(transform.position < outOfBounds)
+        {
+
+        }
+    }
 
     void FixedUpdate ()
     {
@@ -37,24 +54,29 @@ public class PlayerMovement : MonoBehaviour
         Vector3 desiredForward = Vector3.RotateTowards (transform.forward, _Movement, turnSpeed * Time.deltaTime, 0f);
         m_Rotation = Quaternion.LookRotation (desiredForward);
 
-         m_Rigidbody.MovePosition (m_Rigidbody.position + _Movement * movespeed * Time.deltaTime);
-         m_Rigidbody.MoveRotation (m_Rotation);
+         _playerRb.MovePosition (_playerRb.position + _Movement * movespeed * Time.deltaTime);
+         _playerRb.MoveRotation (m_Rotation);
 
-         if(Input.GetKeyDown(KeyCode.Space) && IsOnGround)
-        {
-            _playerRb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
-            IsOnGround = false;
-        }
+        
          _playerRb.AddForce(_Movement * movespeed);
 
          _playerRb.AddForce(_Movement);
     }
     private void OnCollisionEnter(Collision collision)
     {
-        IsOnGround = true;
         if (collision.gameObject.CompareTag("Ground"))
         {
             IsOnGround = true;
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == checkointAreaObject)
+        {
+            isAtCheckpoint = true;
+            //Debug.Log(_startingPosition)
+            _startingPosition = checkointAreaObject.transform.position;
+            //Debug.Log(_startingPosition)
         }
     }
 }
--- 3d game example/Assets/Scripts/Spiner.cs
+++ 3d game example/Assets/Scripts/Spiner.cs
@@ -2,17 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spiner : MonoBehaviour
+public class Rotater : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float RotaionSpeed = 45f;
 
+    private float _horizontalInput;
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(new Vector3(45, 0, 0) * RotaionSpeed * Time.deltaTime);
     }
 }
